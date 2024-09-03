cc3511ebd76afd98cd3bd72c62c55e2d7f153907
jspmern <jspmern@gmail.com>
2024-09-03 10:41:22 +0530

    first commit

--- app/models.py
+++ app/models.py
@@ -1,11 +1,21 @@
 from django.db import models
 
 # Create your models here.
+from django.urls import reverse
 
 class School(models.Model):
     sname=models.CharField(max_length=100)
     spricipal=models.CharField(max_length=100)
     slocation=models.CharField(max_length=100)
+
+    def get_absolute_url(self):
+        return reverse('SchoolDetail',kwargs={'pk':self.pk})
+
+
+
+
+
+
     def __str__(self) -> str:
         return self.sname
 
--- app/views.py
+++ app/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import ListView
+from django.views.generic import ListView,TemplateView,DetailView
 from app.models import *
+
+class Home(TemplateView):
+    template_name='app/home.html'
 
 class SchoolList(ListView):
     model=School
@@ -11,7 +14,9 @@ class SchoolList(ListView):
     #queryset=School.objects.filter(sname='Jspiders')
     #template_name='app/school_list.html'
 
-
+class SchoolDetail(DetailView):
+    model=School
+    context_object_name='schoolobject'
 
 
 '''
